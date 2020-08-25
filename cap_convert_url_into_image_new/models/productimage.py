fdafa7f21e0749e504e3594b90f4df211d1dfb1a
captivea-cko <55201842+captivea-cko@users.noreply.github.com>
2020-08-25 09:40:03 -0400

    2

--- cap_convert_url_into_image_new/models/productimage.py
+++ cap_convert_url_into_image_new/models/productimage.py
@@ -2,11 +2,19 @@
 # Part of CAPTIVEA. Odoo 12 EE.
 
 import base64
+import concurrent.futures
+import matplotlib.pyplot as plt
 from odoo import fields, models
 class ProductImage(models.Model):
 	_inherit = 'product.template'
-	def complicated_task(self,line_id):
-		line=self.env['product.template'].browse(line_id)
-		for x in range(1000):
-			line['list_price']=x
-		return(1)
+	# def complicated_task(self,line_id):
+		# line=self.env['product.template'].browse(line_id)
+		# for x in range(1000):
+			# line['list_price']=x
+		# return(1)
+	def complicated_task(self,action,recordset):
+		outs=[]
+		with concurrent.futures.ProcessPoolExecutor() as executor:
+			for model, out in zip(recordset, executor.map(action, recordset)):
+				outs.append([model,out])
+		return(outs)
---
+++ cap_convert_url_into_image_new/models/productimage_bak.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of CAPTIVEA. Odoo 12 EE.
+
+import base64
+from odoo import fields, models
+class ProductImage(models.Model):
+	_inherit = 'product.template'
+	def complicated_task(self,line_id):
+		line=self.env['product.template'].browse(line_id)
+		for x in range(1000):
+			line['list_price']=x
+		return(1)
