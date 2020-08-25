f6c163192a802174c3e5192dc5b6f6690ee888f9
captivea-cko <55201842+captivea-cko@users.noreply.github.com>
2020-08-25 11:34:57 -0400

    17

--- cap_convert_url_into_image_new/models/productimage.py
+++ cap_convert_url_into_image_new/models/productimage.py
@@ -19,6 +19,12 @@ class ProductImage(models.Model):
 		return(1)
 		
 	def complicated_task(self,recordset):
+		def function(line_id):
+			line=self.env['product.template'].browse(line_id['id'])
+			for x in range(1000):
+				line['list_price']=x
+			return(1)
+		
 		results=[]
 		with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor: #was Process
 			# for model, out in zip(recordset, executor.map(function, recordset)):
