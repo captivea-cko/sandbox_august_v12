b5de0ba2e078e65a86bb543316bb6babce593300
captivea-cko <55201842+captivea-cko@users.noreply.github.com>
2020-08-26 11:06:24 -0400

    .

--- cap_convert_url_into_image_new/models/productimage.py
+++ cap_convert_url_into_image_new/models/productimage.py
@@ -13,20 +13,20 @@ class ProductImage(models.Model):
 			line['list_price']=x
 		return(1)
 	
-	# @api.multi
-	# def complicated_task(self,my_action,list):
-		# results=[]
-		# with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: #was Process
-			# # for model, out in zip(recordset, executor.map(function, recordset)):
+	@api.multi
+	def complicated_task(self,my_action,list):
+		results=[]
+		with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: #was Process
+			# for model, out in zip(recordset, executor.map(function, recordset)):
 				# # outs.append([model,out])
-			# futures = [executor.submit(my_action, el) for el in list]
+			futures = [executor.submit(my_action, el) for el in list]
 			# for future in concurrent.futures.as_completed(futures):
 				# el=futures[future]
 				# # try:
 					# # results.append(future.result())
 				# # except Exception as exc:
 				# print('%r generated an exception: %s' % (el)
-		# return(results)
+		return(futures)
 			
 		# return(outs)
 		# results = []
